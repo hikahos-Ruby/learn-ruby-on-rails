4ea934f7551d084f96379ceb7ab221595f0f9f79
Alan Fajayan <alan.fajayan@gmail.com>
2018-07-21 01:10:23 +0000

    Added devise gem

--- Gemfile
+++ Gemfile
@@ -39,6 +39,8 @@ gem 'bootsnap', '>= 1.1.0', require: false
 gem 'jquery-rails', '~> 4.3', '>= 4.3.3'
 
 gem 'bootstrap-sass', '~> 3.3', '>= 3.3.7'
+gem "stock_quote", '~> 2.0.0'
+gem 'devise', '~> 4.4', '>= 4.4.3'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,16 @@
 class HomeController < ApplicationController
   def index
+    if params[:id] == ""
+      @nothing = "Hey, you forgot to enter a symbol"
+    elsif
+      if params[:id]
+        begin
+          @stock = StockQuote::Stock.quote(params[:id])
+        rescue StandardError
+          @error = "That Stock Symbol doesn't exist... please try again."
+        end
+      end
+    end
   end
   
   def about
