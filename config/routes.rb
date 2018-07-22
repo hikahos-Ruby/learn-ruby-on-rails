4ebfa6c6f48360c2d917cde49901506ad3383132
Alan Fajayan <alan.fajayan@gmail.com>
2018-07-22 00:02:36 +0000

    Modified gemfile for pg

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # get 'home/index'
   root 'home#index'
   get 'home/about'
-  
   post "/" => 'home#index'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
